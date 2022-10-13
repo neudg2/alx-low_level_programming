4efe61104a3cbf7e9f381f59c604e2d6fb9d3c2b
neudg2 <neudg2@gmail.com>
2022-10-13 13:18:44 -0700

    low level pro first try

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main -Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like buiding a multilingual puzzle");
+		return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
