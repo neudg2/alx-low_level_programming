83d9db3aca5815fa35f4ef53af565cf0cb841c2c
neudg2 <neudg2@gmail.com>
2022-10-13 13:23:54 -0700

    task 3, 4, 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main -Entry
- * Return: Always 0
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like buiding a multilingual puzzle");
-		return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
