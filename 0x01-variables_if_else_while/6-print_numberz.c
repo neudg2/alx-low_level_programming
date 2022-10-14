08474524a955092c54412ffeb7a3fc8ce26c8503
neudg2 <neudg2@gmail.com>
2022-10-14 14:07:56 -0700

    task 6 done correctly

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,15 @@
 
 int main(void)
 {
-int i;
 
-for (i=0; i<10 ; i++)
-{
-putchar('0'+ i);
-}
-putchar('\n');
-return(0);
-}
+	int i;
+
+	for (i=0; i<10 ; i++)
+	{
+	putchar('0'+ i);
+	
+	}
+	putchar('\n');
+	return(0);
+
+	}
