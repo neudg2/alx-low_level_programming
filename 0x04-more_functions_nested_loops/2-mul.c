99082f9c0ae0992aad9bdfe1273793c03354c16d
neudg2 <neudg2@gmail.com>
2022-10-20 13:06:35 -0700

    more funtion tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - return multiplication of a * b
+ * @a: first number to multiply
+ * @b: secondary number to multiply
+ * Return: multiplication of a * b
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
