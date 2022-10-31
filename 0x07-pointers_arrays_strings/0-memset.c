48c816f870d52008059af28ab4d6d2c38b3b9d0d
neudg2 <neudg2@gmail.com>
2022-10-31 12:29:43 -0700

    sec try

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * *_memset - concatenates two strings
- * @s:  pointer
- * @b: constant byte
- * @n: n bytes of memory
- * Return: s
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0 ; i++, n--)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
-
 	return (s);
 }
